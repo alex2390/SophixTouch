8d7a7019ce929ed5254142f0f653b0925ad0032d
renjiawen <renjiawen@isoubu.com>
2018-11-14 18:02:09 +0800

    init

---
+++ app/src/main/java/com/example/rjw/sophixtouch/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.rjw.sophixtouch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.tv);
+
+        textView.setText("bug修复了666");
+
+    }
+}
---
+++ app/src/main/java/com/example/rjw/sophixtouch/MyApplication.java
@@ -0,0 +1,16 @@
+package com.example.rjw.sophixtouch;
+
+import android.app.Application;
+
+/**
+ * Desc:
+ * <p>
+ * Created by renjiawen on 2018/11/14.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
